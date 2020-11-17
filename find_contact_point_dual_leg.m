af1db75123dd3f24c583f309b86d5b0228be94dc
Ting-Hao Wang <w.tyng.h@gmail.com>
2020-11-17 16:23:17 +0800

    Add files via upload
    "main" and "entrance and function" are included.

--- find_contact_point_dual_leg.m
+++ find_contact_point_dual_leg.m
@@ -6,36 +6,22 @@ function contact_point = find_contact_point(leg_contour , landscape_table , r)
 
 
     contact_area_index_1 = leg_contour.leg_1.contour.y <= interp1(landscape_table(1,:),landscape_table(2,:),leg_contour.leg_1.contour.x);
-    contact_area_index_2 = leg_contour.leg_2.contour.y <= interp1(landscape_table(1,:),landscape_table(2,:),leg_contour.leg_2.contour.x);
-    
+    contact_area_index_2 = leg_contour.leg_2.contour.y <= interp1(landscape_table(1,:),landscape_table(2,:),leg_contour.leg_2.contour.x);   
+    contact_area_index_3 = leg_contour.leg_3.contour.y <= interp1(landscape_table(1,:),landscape_table(2,:),leg_contour.leg_3.contour.x);
+    contact_area_index_4 = leg_contour.leg_4.contour.y <= interp1(landscape_table(1,:),landscape_table(2,:),leg_contour.leg_4.contour.x);
     % find all the points of contour lower than the landscape
     
     leg_1_touch_gound = ~isempty( find(contact_area_index_1, 1) ) ;
     leg_2_touch_gound = ~isempty( find(contact_area_index_2, 1) ) ;
-
+    leg_3_touch_gound = ~isempty( find(contact_area_index_3, 1) ) ;
+    leg_4_touch_gound = ~isempty( find(contact_area_index_4, 1) ) ;
 
     if leg_1_touch_gound
         % define searching area of x of the landscape
         contact_area_x_1 = leg_contour.leg_1.contour.x( 1, contact_area_index_1 );
 
         contact_area_y_1 = interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_1);
-% 
-%         % find the lowest point
-%         [min_dif_1,min_dif_index_1] = ...
-%          min( leg_contour.leg_1.contour.y(1, contact_area_index_1) - interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_1));
-% 
-%         contact_point_x = contact_area_x_1(min_dif_index_1);
-% 
-%         contact_point_1 = [contact_point_x, interp1(landscape_table(1,:),landscape_table(2,:),contact_point_x)];
-% 
-% 
-%         revise_vector_1 = (leg_contour.leg_1.center - contact_point_1) / norm(leg_contour.leg_1.center - contact_point_1);
-%         revise_vector_1 = revise_vector_1 * min_dif_1;
-% 
-%         contact_point.point_1 = [contact_point_1 , revise_vector_1];
-         
 
-     %%%%%%%% solution 2
         landscape_points_1 = [contact_area_x_1 ; contact_area_y_1];   
         ditance_matrix_1 = pdist2 (leg_contour.leg_1.center , landscape_points_1'  );
         % ditance_matrix(i,j) 
@@ -65,19 +51,6 @@ function contact_point = find_contact_point(leg_contour , landscape_table , r)
         contact_area_x_2 = leg_contour.leg_2.contour.x( 1, contact_area_index_2 );
         contact_area_y_2 = interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_2);
 
-%         [min_dif_2,min_dif_index_2] = ...
-%          min( leg_contour.leg_2.contour.y(1, contact_area_index_2) - interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_2));
-%         contact_point_x = contact_area_x_2(min_dif_index_2);
-%         
-%         contact_point_2 = [contact_point_x, interp1(landscape_table(1,:),landscape_table(2,:),contact_point_x) ];
-%         
-%         revise_vector_2 = (leg_contour.leg_2.center - contact_point_2) / norm(leg_contour.leg_2.center - contact_point_2);
-%         revise_vector_2 = revise_vector_2 * min_dif_2;
-%         
-%         contact_point.point_2 = [contact_point_2 , revise_vector_2];
-
-
-    %%%%%%%% solution 2
         landscape_points_2 = [contact_area_x_2 ; contact_area_y_2];   
         ditance_matrix_2 = pdist2 ( leg_contour.leg_2.center , landscape_points_2'  );
         % ditance_matrix(i,j) 
@@ -97,8 +70,61 @@ function contact_point = find_contact_point(leg_contour , landscape_table , r)
         contact_point.point_2.point = [];
         contact_point.point_2.revise = [];
     end
+    
+    if leg_3_touch_gound
+        % define searching area of x of the landscape
+        contact_area_x_3 = leg_contour.leg_3.contour.x( 1, contact_area_index_3 );
 
+        contact_area_y_3 = interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_3);
 
+        landscape_points_3 = [contact_area_x_3 ; contact_area_y_3];   
+        ditance_matrix_3 = pdist2 (leg_contour.leg_3.center , landscape_points_3'  );
+        % ditance_matrix(i,j) 
+        % means the distance from ith point in X to jth point in Y
+        
+        % Find the min_dis between the half circle center and the ground
+        min_distance_3 = min(ditance_matrix_3(:));
+        [i,j] = find(ditance_matrix_3 == min_distance_3 , 1);
+        
+        revise_vector_3 = (leg_contour.leg_3.center - landscape_points_3(:,j)')/norm(leg_contour.leg_3.center - landscape_points_3(:,j)');
+        revise_vector_3 = revise_vector_3*(r - min_distance_3);
+        
+        contact_point.point_3.point = landscape_points_3(:,j)';
+        contact_point.point_3.revise = revise_vector_3;
+
+        %ditance between contact point and tip & toe
+        contact_point.point_3.istip = norm(landscape_points_3(:,j)' - leg_contour.leg_3.tip ) < error_dis;
+        contact_point.point_3.istoe = norm(landscape_points_3(:,j)' - leg_contour.leg_3.toe ) < error_dis;
+ 
+    else
+        contact_point.point_3.point = [];
+        contact_point.point_3.revise = [];
+    end
+
+    if leg_4_touch_gound
+
+        contact_area_x_4 = leg_contour.leg_4.contour.x( 1, contact_area_index_4 );
+        contact_area_y_4 = interp1(landscape_table(1,:),landscape_table(2,:),contact_area_x_4);
+
+        landscape_points_4 = [contact_area_x_4 ; contact_area_y_4];   
+        ditance_matrix_4 = pdist2 ( leg_contour.leg_4.center , landscape_points_4'  );
+        % ditance_matrix(i,j) 
+        % means the distance from ith point in X to jth point in Y
+        
+        min_distance_4 = min(ditance_matrix_4(:));
+        [u,v] = find(ditance_matrix_4 == min_distance_4 , 1);
+        revise_vector_4 = (leg_contour.leg_4.center - landscape_points_4(:,v)') / norm(leg_contour.leg_4.center - landscape_points_4(:,v)');
+        revise_vector_4 = revise_vector_4 * (r - min_distance_4);
+        contact_point.point_4.point = landscape_points_4(:,v)';
+        contact_point.point_4.revise = revise_vector_4;
+
+        %ditance between contact point and tip & toe
+        contact_point.point_4.istip = norm(landscape_points_4(:,v)' - leg_contour.leg_4.tip ) < error_dis;
+        contact_point.point_4.istoe = norm(landscape_points_4(:,v)' - leg_contour.leg_4.toe ) < error_dis;
+    else
+        contact_point.point_4.point = [];
+        contact_point.point_4.revise = [];
+    end
 end
 
 
